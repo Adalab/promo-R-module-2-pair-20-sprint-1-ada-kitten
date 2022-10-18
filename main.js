ad6fb58c04b081a1504eacbaf62f0ec31f394182
Laura Garcia <lgarciacuello@gmail.com>
2022-10-18 11:14:44 +0200

    class JS

--- main.js
+++ main.js
@@ -1,2 +1,56 @@
 "use strict";
-document.querySelector(".js/new/form")
+document.querySelector('.js-new-form')
+classList.remove("collapsed")
+
+document.queryselector(".js-list") 
+
+/* Agregar el código del li desde HTMl 
+Repetir este proceso por cada gatito */
+
+const kittenOne = document.querySelector (".js-list")
+List.innerHTML= 
+`<li class="card">
+<article>
+<img
+class="card_img"
+src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
+alt="gatito"
+/>
+<h3 class="card_title">Anastacio</h3>
+<p class="card_description">
+Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+moleste. Es una maravilla acariciarle!
+</p>
+</article>
+</li>`; 
+
+const kittenTwo = `<li class="card">
+<article>
+<img
+class="card_img"
+src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
+alt="gatito"
+/>
+<h3 class="card_title">Anastacio</h3>
+<p class="card_description">
+Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+moleste. Es una maravilla acariciarle!
+</p>
+</article>
+</li>`; 
+
+const kittenThree = `<li class="card">
+<article>
+<img
+class="card_img"
+src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
+alt="gatito"
+/>
+<h3 class="card_title">Anastacio</h3>
+<p class="card_description">
+Ruiseño, juguetón, le guta estar tranquilo y que nadie le
+moleste. Es una maravilla acariciarle!
+</p>
+</article>
+</li>`; 
+
