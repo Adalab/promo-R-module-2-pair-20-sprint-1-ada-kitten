0dc611bfd647aaeff0d144d8fff38b37e0cbf389
Ameliach9 <ale.ameliach@gmail.com>
2022-10-20 11:16:34 +0200

    js clase 2

--- js/main.js
+++ js/main.js
@@ -24,6 +24,27 @@ const kittenThreeName = 'Cielo';
 const kittenThreeDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!'
 const kittenThreeRace = 'British Shorthair'
 
+const input_search_desc = document.querySelector('.js_in_search_desc');
+input_search_desc.value = 'cariñoso';
+
+
+const descrSearchText = input_search_desc.value;
+
+if( kittenOneDesc.includes(descrSearchText) ) { 
+    list.innerHTML = kittenOne
+} 
+    
+    if( kittenTwoDesc.includes(descrSearchText) ) {
+    list.innerHTML = kittenTwo
+    
+ }
+    
+    if( kittenThreeDesc.includes(descrSearchText) ) {
+    list.innerHTML = kittenThree
+  
+    }
+
+
 const kittenOne = 
 `<li class="card"> <article> <img class="card_img" src=  ${kittenOneImage}  alt="gatito" /> <h3 class="card_title"> ${kittenOneName} </h3> <h4 class="card_race"> ${kittenOneRace} </h4> <p class="card_description"> ${kittenOneDesc} </p></article></li>`;
 
