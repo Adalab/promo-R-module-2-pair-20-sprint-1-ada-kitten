6ebb75a4314e7d154008ca4c804820bd3d7b8faf
Laura Garcia <lgarciacuello@gmail.com>
2022-10-24 15:48:37 +0200

    exercise 4

--- js/main.js
+++ js/main.js
@@ -4,10 +4,20 @@ const formElement = document.querySelector('.js-new-form');
 
 formElement.classList.remove("collapsed")
 
-document.querySelector(".js-list") 
+const icon = document.querySelector ('.js-icon')
 
 
 const list = document.querySelector(".js-list")
+const btnAdd = document.querySelector (".js-btn-add")
+const inputDesc = document.querySelector (".js-input-desc")
+const inputPhoto = document.querySelector (".js-input-photo")
+const inputName = document.querySelector (".js-input-name")
+const labelMesageError = document.querySelector('.js-label-error'); 
+const valueDesc = inputDesc.value;
+const valuePhoto = inputPhoto.value;
+const valueName = inputName.value;
+const btnRemove = document.querySelector (".js-btn-remove")
+
 
 const kittenOneImage= 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg' ;
 const kittenOneName = 'Anastacio';
@@ -23,27 +33,6 @@ const kittenThreeImage= 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg' ;
 const kittenThreeName = 'Cielo';
 const kittenThreeDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!'
 const kittenThreeRace = 'British Shorthair'
-
-const input_search_desc = document.querySelector('.js_in_search_desc');
-input_search_desc.value = 'cariñoso';
-
-
-const descrSearchText = input_search_desc.value;
-
-if( kittenOneDesc.includes(descrSearchText) ) { 
-    list.innerHTML += kittenOne
-} 
-    
-    if( kittenTwoDesc.includes(descrSearchText) ) {
-    list.innerHTML += kittenTwo
-    
- }
-    
-    if( kittenThreeDesc.includes(descrSearchText) ) {
-    list.innerHTML += kittenThree
-  
-    }
-
 
 const kittenOne = 
 `<li class="card"> <article> <img class="card_img" src=  ${kittenOneImage}  alt="gatito" /> <h3 class="card_title"> ${kittenOneName} </h3> <h4 class="card_race"> ${kittenOneRace} </h4> <p class="card_description"> ${kittenOneDesc} </p></article></li>`;
@@ -73,4 +62,68 @@ ${kittenThreeDesc}
 </li>`; 
 
 list.innerHTML += kittenOne + kittenTwo + kittenThree;
+
+const input_search_desc = document.querySelector('.js_in_search_desc');
+input_search_desc.value = 'cariñoso';
+
+
+const descrSearchText = input_search_desc.value;
+
+if( kittenOneDesc.includes(descrSearchText) ) { 
+    list.innerHTML += kittenOne
+} 
+    
+    if( kittenTwoDesc.includes(descrSearchText) ) {
+    list.innerHTML += kittenTwo
+    
+ }
+    
+    if( kittenThreeDesc.includes(descrSearchText) ) {
+    list.innerHTML += kittenThree
+  
+    }
+
+    // event.preventDefault() 
+
+
+
+icon.addEventListener (`click`, (event) => {
+    console.log (`he hecho click`);
+    event.preventDefault ();
+    if (formElement.classList.contains('collapsed')) {
+        formElement.classList.remove ('collapsed')
+    } else {
+        formElement.classList.add ('collapsed')}
+    
+});
+
+
+btnAdd.addEventListener (`click`, (ev) => {
+    console.log (`click añadir`)
+    ev.preventDefault ();
+
+    if (valueDesc === '' || valuePhoto === '' || valueName === '')
+    {
+        labelMesageError.innerHTML = 'Debe rellenar todos los valores';
+    }        
+    
+//   ultimo ejercicio cancelar y guardar formulario    falta que se borre el formulario  
+   
+});
+
+btnRemove.addEventListener (`click`, (ev) => {
+    console.log (`click añadir`)
+    ev.preventDefault ();
+    formElement.classList.remove ('collapsed')
+
+    if (formElement.classList.contains('collapsed')) {
+        formElement.classList.remove ('collapsed')
+        
+    } else {
+        formElement.classList.add ('collapsed')}
+     
+    
+        
+   
+});
 
