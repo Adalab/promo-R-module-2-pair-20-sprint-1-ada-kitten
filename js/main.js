406c7baedf6e808bda4006be43b9c5fffb29a4f0
Laura Garcia <lgarciacuello@gmail.com>
2022-10-25 10:31:53 +0200

    change

--- js/main.js
+++ js/main.js
@@ -34,27 +34,6 @@ const kittenThreeName = 'Cielo';
 const kittenThreeDesc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!'
 const kittenThreeRace = 'British Shorthair'
 
-const input_search_desc = document.querySelector('.js_in_search_desc');
-input_search_desc.value = 'cariñoso';
-
-
-const descrSearchText = input_search_desc.value;
-
-if( kittenOneDesc.includes(descrSearchText) ) { 
-    list.innerHTML += kittenOne
-} 
-    
-    if( kittenTwoDesc.includes(descrSearchText) ) {
-    list.innerHTML += kittenTwo
-    
- }
-    
-    if( kittenThreeDesc.includes(descrSearchText) ) {
-    list.innerHTML += kittenThree
-  
-    }
-
-
 const kittenOne = 
 `<li class="card"> <article> <img class="card_img" src=  ${kittenOneImage}  alt="gatito" /> <h3 class="card_title"> ${kittenOneName} </h3> <h4 class="card_race"> ${kittenOneRace} </h4> <p class="card_description"> ${kittenOneDesc} </p></article></li>`;
 
@@ -82,22 +61,7 @@ ${kittenThreeDesc}
 </article>
 </li>`; 
 
-
-
-if( kittenOneDesc.includes(descrSearchText) ) { 
-    list.innerHTML += kittenOne
-} 
-    
-    if( kittenTwoDesc.includes(descrSearchText) ) {
-    list.innerHTML += kittenTwo
-    
- }
-    
-    if( kittenThreeDesc.includes(descrSearchText) ) {
-    list.innerHTML += kittenThree
-  
-    }
-
+list.innerHTML += kittenOne + kittenTwo + kittenThree;
 
 const input_search_desc = document.querySelector('.js_in_search_desc');
 input_search_desc.value = 'cariñoso';
